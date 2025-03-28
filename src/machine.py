f0982ec3136dbc85b14f4665f73e6265f484162d
Philip <philpm2023@gmail.com>
2025-03-28 18:16:42 +0300

    adding old_files to gitignore

--- src/machine.py
+++ src/machine.py
@@ -1,18 +1,18 @@
+from pydantic import BaseModel,Field,IPvAnyAddress,ValidationError
 import logging
 import json
 import os
 
 
 logging.basicConfig(
-            filename="logs/machine.log",
+            filename="logs/provisioning.log",
             level=logging.INFO,
             format="%(asctime)s - %(levelname)s - %(message)s", 
             )
 
 
 def save_to_json(data):
-        """Save machine data to configs/instances.json."""
-
+        
         CONFIG_FILE = "configs/instances.json"
 
         # 1. Ensure the 'configs' folder exists
@@ -35,46 +35,94 @@ def save_to_json(data):
             with open(CONFIG_FILE, "w") as file:
                 json.dump(content, file, indent=4)  # Pretty-print JSON
                 logging.info(f"Data successfully saved to {CONFIG_FILE}")
-                print("\nSaving VMachine configuration...")
-                print(f"Saved to {CONFIG_FILE}")
+                print("\nVM configured successfully")
+                print(f"VM Saved to {CONFIG_FILE}")
                 print("Done!")
         except Exception as e:
                 logging.error(f"Error saving to {CONFIG_FILE}: {e}")
                 print("Error saving the file.")
 
 
-class VM():
-    def __init__(self,Name,OS,CPU,GPU,RAM,Disk,IP):
-        self.Name = Name
-        self.OS   = OS
-        self.CPU  = CPU
-        self.GPU  = GPU
-        self.RAM  = RAM
-        self.Disk = Disk
-        self.IP   = IP
 
-        logging.info(f"VMachine Created : {self.to_dict()}")  # every time a single machine is created, it has a record log.
-        save_to_json([self.to_dict()])                       # every time a single machine is created, it is saved in json and stored in instances.json.
-                                                             # I dont need to wait every time i press 'no', so a machine will be loged and saved, it is now doing it immediatly, when a class is being called.
+class VMachine(BaseModel):
 
-        print("\nVMachine Created !  ")
-        print("\nVMachine Details : \n")
-
-        for key,value in self.to_dict().items():  # Showing a more readable VM info on the terminal(stdout).
-            print(f"{key} : {value}")  
+    Name : str = Field(...,pattern = r'^[a-zA-Z0-9-_]+$',description ="Name format must be : [ VM name - number]")
+    OS : str = Field(...,pattern=r'^(Windows|Linux|Mac)$', description = "Operating-System must be : ( Windows, Linux, Mac )")
+    CPU : int = Field(...,ge=1,le=64,description="CPU must be between : 1 - 64")
+    GPU : str = Field(...,pattern=r'^(Nvidia|AMD|Intel)$',description = " GPU must be : ( Nvidia, AMD, Intel )")
+    RAM : int = Field(...,ge=1,le=256,description="RAM Memory must be (0 - 256)GB")
+    Disk : int = Field(...,ge=1,le=500,description="Disk Storage must be (0 - 500)GB)")
+    IP : IPvAnyAddress 
 
 
-
-        # Return the Machine details as a dictionary - 'to_dict' converts the object's attributes into a dictionary for easy storage and display
     def to_dict(self):               
-        return {
-            "Name":self.Name,
-            "OS"  :self.OS,
-            "CPU" :self.CPU,
-            "GPU" :self.GPU,
-            "RAM" :f"{self.RAM}GB",
-            "Disk":f"{self.Disk}GB",
-            "IP"  :str(self.IP)
-        }
+                return {
+                    "Name":self.Name,
+                    "OS"  :self.OS,
+                    "CPU" :self.CPU,
+                    "GPU" :self.GPU,
+                    "RAM" :f"{self.RAM}GB",
+                    "Disk":f"{self.Disk}GB",
+                    "IP"  :str(self.IP)
+                }
+
+
+
+    # In Pydantic v2, the regex parameter has been replaced by the 'pattern' parameter in the Field() function.
+ 
+def get_vm_input():
+        
+    try:       
+            #  collect user input
+        Name = input("\nEnter a valid VM name in the format : [ VM name - number]: ").strip()
+        OS = input("Choose an Operating-System : ( Windows, Linux, Mac ) : ").strip()
+        CPU = int(input("Enter the number of cores of the CPU ( 1 - 64 ) : "))
+        GPU = input("Choose GPU vendor ( Nvidia, AMD, Intel ) :  ").strip()
+        RAM = int(input("Enter RAM Memory in GB (0 - 256) :  "))
+        Disk = int(input("Enter Disk Storage in GB (0 - 500) : "))
+        IP = input("Enter an IP Adress ( 0-255.0-255.0-255.0-255 ) : ")  # i removed 'int' because pydantuc handles with string automatically.
+
+            # validate using pydantic
+        vm = VMachine(Name=Name,OS=OS,CPU=CPU,GPU=GPU,RAM=RAM,Disk=Disk,IP=IP)
+
+        logging.info(f"VMachine Created : {vm.to_dict()}") # every time a single machine is created, it has a record log.
+        save_to_json([vm.to_dict()])                       # every time a single machine is created, it is saved in json and stored in instances.json.
+                                                                 # I dont need to wait every time i press 'no', so a machine will be loged and saved, it is now doing it immediatly, when a class is being called.
+
+            #print("\nVM Created Successfully:\n", vm)
+        return vm
+
+
+    except ValidationError as e:
+        logging.error(f"Validation error(s) occurred: {e}")
+        print(f"\nValidation Error(s):")
+        for error in e.errors():
+            print(f"- {error['loc']}: {error['msg']}")
+
     
+    # except ValueError as e:
+    #     #print(f"error : {e}")
+    #     print("\nInvalid input! Please enter numbers where required.")
+
+    # except TypeError as e:
+    #     #print(f"error : {e}")
+    #     print("\nUnexpected input type! Please follow the required format.")
+
+
+
+#Pydantic serves both as a class definition and a validation framework at the same time!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- src/main.py
+++ src/main.py
@@ -1,74 +1,97 @@
-from validation import get_vm_input #,Name_Validation,OS_Validation,CPU_Validation,GPU_Validation,RAM_Validation,Disk_Validation,IP_Validation
+from machine import get_vm_input,logging
 
-from machine import VM
+def main():
 
+    VMs = []  # Store multiple VM configurations in a List.
+    count = 0 # Number of VMs created.
 
-VMs = []  # Store multiple VM configurations in a List.
-count = 0 # Number of VMs created.
+    while True:
+        answer = input("\nDo you want to create a New VMachine ? ( Yes / No ) : ").strip().lower()
 
-while True:
-    answer = input("\nDo you want to create a New VMachine ? ( Yes / No ) : ").strip().lower()
+        if answer == "yes":
+            logging.info("-" * 100)
+            logging.info("==== Session Started ====")
+            logging.info("User chose to create a new VMachine.")
+            print("\nPlease enter the next details of a new VMachine :")
+            
+            
+            data = get_vm_input()
 
-    if answer == "yes":
-
-        print("\nPlease enter the next details of a new VMachine :")
-        
-        data = get_vm_input()
-        if data:
-            #Create a new VM object using the validated data
-            vm = VM(data.Name,data.OS,data.CPU,data.GPU,data.RAM,data.Disk,data.IP)
-
-        #print("\nPlease enter the next details of a new VMachine :")
-
-        # Name = Name_Validation(input("\nEnter a valid VM name in the format : [ VM name - number]: ").strip())
-        # OS = OS_Validation(input("Choose an Operating-System : ( Windows, Linux, Mac ) : ").strip())
-        # CPU = CPU_Validation(input("Enter the number of cores of the CPU ( 1 - 64 ) : "))
-        # GPU = GPU_Validation(input("Choose GPU vendor ( Nvidia, AMD, Intel ) :  ").strip())
-        # RAM = RAM_Validation(input("Enter RAM Memory in GB (0 - 256) :  ").strip())
-        # Disk = Disk_Validation(input("Enter Disk Storage in GB (0 - 500) : ").strip())
-        # IP = IP_Validation(input("Enter an IP Adress ( 0-255.0-255.0-255.0-255 ) : ").strip())
+            if data:
+                count += 1
+                VMs.append(data)  # Store the dictionary representation of the VM
+                logging.info(f"VM #{count} created with details: {data}")
 
 
-        #Conversion from Machine Class to Dictionary.
-            vm_dict = vm.to_dict()    # Dictionary Conversion for Machine: You’re trying to convert the Machine object into a dictionary using dict(vm), 
-                                      # but this might not work because vm is an instance of Machine, 
-                                      # and the dict() function expects the object to have a __dict__ method or an iterable that returns key-value pairs. 
-                                      # Since you’ve already defined the to_dict() method, you should call vm.to_dict() instead of using dict(vm).
+            # if data:
+            #     #Create a new VM object using the validated data
+            #     vm = VMachine(data.Name,data.OS,data.CPU,data.GPU,data.RAM,data.Disk,data.IP)
 
-                                      # 'to_dict' is-not mensioned above, because 'Machine' class already contains it.
+            #print("\nPlease enter the next details of a new VMachine :")
+
+            # Name = Name_Validation(input("\nEnter a valid VM name in the format : [ VM name - number]: ").strip())
+            # OS = OS_Validation(input("Choose an Operating-System : ( Windows, Linux, Mac ) : ").strip())
+            # CPU = CPU_Validation(input("Enter the number of cores of the CPU ( 1 - 64 ) : "))
+            # GPU = GPU_Validation(input("Choose GPU vendor ( Nvidia, AMD, Intel ) :  ").strip())
+            # RAM = RAM_Validation(input("Enter RAM Memory in GB (0 - 256) :  ").strip())
+            # Disk = Disk_Validation(input("Enter Disk Storage in GB (0 - 500) : ").strip())
+            # IP = IP_Validation(input("Enter an IP Adress ( 0-255.0-255.0-255.0-255 ) : ").strip())
 
 
-        # Inserting Data into Dictionary.
-        # VM_info["Name"] = Name
-        # VM_info["OS"] = OS
-        # VM_info["CPU"] = CPU
-        # VM_info["GPU"] = GPU
-        # VM_info["RAM"] = f"{RAM}GB"
-        # VM_info["Disk"] = f"{Disk}GB"
-        # VM_info["IP"] = str(IP) # without 'str', an error will accur : 'Object of type IPv4Address is not JSON serializable.'
+            #Conversion from Machine Class to Dictionary.
+                # vm_dict = vm.to_dict()    # Dictionary Conversion for Machine: You’re trying to convert the Machine object into a dictionary using dict(vm), 
+                                        # but this might not work because vm is an instance of Machine, 
+                                        # and the dict() function expects the object to have a __dict__ method or an iterable that returns key-value pairs. 
+                                        # Since you’ve already defined the to_dict() method, you should call vm.to_dict() instead of using dict(vm).
 
-        #count += 1
-            VMs.append(vm_dict) # VMs will store the Machine object (not its attributes). 
-                            # I want to append a dictionary representation of the object (such as the result of the to_dict() method).
-            count += 1
+                                        # 'to_dict' is-not mensioned above, because 'Machine' class already contains it.
+
+
+            # Inserting Data into Dictionary.
+            # VM_info["Name"] = Name
+            # VM_info["OS"] = OS
+            # VM_info["CPU"] = CPU
+            # VM_info["GPU"] = GPU
+            # VM_info["RAM"] = f"{RAM}GB"
+            # VM_info["Disk"] = f"{Disk}GB"
+            # VM_info["IP"] = str(IP) # without 'str', an error will accur : 'Object of type IPv4Address is not JSON serializable.'
+
+           
+            # count += 1
+            # VMs.append(data) # VMs will store the Machine object (not its attributes). 
+            #                     # I want to append a dictionary representation of the object (such as the result of the to_dict() method).
+
+            # logging.info(f"VM #{count} created with details: {data}")
+
+
+        elif answer == "":
+            logging.warning("No input received, user prompted again.")
+            print("\nNothing was inserted, Please enter 'Yes' or 'No' !")
 
         else:
-            print("Invalid data received. Please try again.")
+            logging.info("Configuration canceled by user.")
+            print("\nConfiguration Canceled !")
+            break
 
-    elif answer == "":
-        print("\nNothing was inserted, Please enter 'Yes' or 'No' !")
+     
 
+    logging.info(f"{count} VMs created.")
+    logging.info("===== Session Ended =====")
+    logging.info("-" * 100)
+    
+    print(f"\n{count} VM(s) created !\n")
+
+    if count > 0:
+        print("\nFinal List of Created VM(s):")
+        for i in range(len(VMs)):                     # printing each VM only once with its index.
+            print(f"VM No: {i+1} : {VMs[i]}\n")
     else:
-        print("\nConfiguration Canceled !")
-        break
+        None
+
+    # # Saves only if VMs list exists
+    # if VMs:
+    #     save_to_json(VMs)  
 
 
-print(f"\n{count} VMs created !")
-print("\nFinal List of Created VMs:")
-
-for i in range(len(VMs)):                     # printing each VM only once with its index.
-    print(f"VM num: {i+1} : {VMs[i]}")
-
-# # Saves only if VMs list exists
-# if VMs:
-#     save_to_json(VMs)  
+if __name__ == "__main__":
+    main()
